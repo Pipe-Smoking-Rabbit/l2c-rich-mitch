364213b845a54d83f704a3c581d5050a65e2ea95
Richard Neat <richardneat@richards-mbp.home>
2023-01-17 14:54:50 +0000

    seed prod

--- db/seed/run-seed.js
+++ db/seed/run-seed.js
@@ -5,9 +5,10 @@ const fs = require("fs");
 
 const data = fs.readFileSync(`${__dirname}/../../Lesson.md`, "utf-8");
 const tokens = marked.lexer(data);
+const stringTokens = JSON.stringify(tokens);
 
 const runSeed = () => {
-  return seed(JSON.stringify(tokens)).then(() => db.end());
+  return seed(stringTokens).then(() => db.end());
 };
 
 runSeed();
